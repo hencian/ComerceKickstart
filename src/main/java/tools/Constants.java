400dedfcbfcd7fab85e4dbe24c2e4ce5afcbd76b
MonaIancu <monalisa.iancu@yahoo.com>
2017-05-04 17:18:04 +0300

    relativ paths added

--- src/main/java/tools/Constants.java
+++ src/main/java/tools/Constants.java
@@ -2,8 +2,8 @@ package tools;
 
 public class Constants {
 	public static final String BROWSER = "chrome";
-	public static final String FIREFOX_PATH = "D:\\Java\\Practice\\Automation\\MavenTestProject\\drivers\\geckodriver.exe";
-	public static final String CHROME_PATH = "D:\\Java\\Practice\\Automation\\MavenTestProject\\drivers\\chromedriver.exe";
+	public static final String FIREFOX_PATH = "drivers/geckodriver.exe";
+	public static final String CHROME_PATH = "drivers/chromedriver.exe";
 	
 	public static final String HOST = "http://172.22.120.55:8080/";
 	
--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -1,27 +1,19 @@
 package test;
-import org.junit.After;
-import org.junit.Before;
+
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import page.CommerceHeaderPage;
 import page.CommerceLoginPage;
 import tools.Constants;
 
-public class LoginTest extends BaseTest{
-	
-	
+public class LoginTest extends BaseTest {
+
 	String username = Constants.USERNAME;
 	String password = Constants.PASSWORD;
-	
-	
-	
-	@Test
-	public void Login(){
 
-//		System.setProperty("webdriver.firefox.marionette", "/./drivers/geckodriver.exe");
-		
+	@Test
+	public void Login() {
+
 		CommerceHeaderPage headerPage = new CommerceHeaderPage(driver);
 		CommerceLoginPage loginPage = new CommerceLoginPage(driver);
 
@@ -29,5 +21,5 @@ public class LoginTest extends BaseTest{
 		loginPage.login(username, password);
 		loginPage.ClickLoginButton();
 	}
-	
+
 }
